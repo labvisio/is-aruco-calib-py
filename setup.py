df2933115187adc08d37e2ce658960f0e72298c5
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-10-27 17:17:45 -0300

    style: format setup.py file

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='is_aruco_calib',
-    version='0.0.1',
-    description='',
-    url='http://github.com/labvisio/is-aruco-calib-py',
-    author='labvisio',
-    license='MIT',
-    packages=find_packages('.'),
-    package_dir={'': '.'},
+    name="is_aruco_calib",
+    version="0.0.1",
+    description="",
+    url="http://github.com/labvisio/is-aruco-calib-py",
+    author="labvisio",
+    license="MIT",
+    packages=find_packages("."),
+    package_dir={"": "."},
     entry_points={
-        'console_scripts': [
-            'is-aruco-calib-intrinsic=is_aruco_calib.calibrate.intrinsic:main',
-            'is-aruco-calib-extrinsic=is_aruco_calib.calibrate.extrinsic:main',
-            'is-aruco-calib-marker=is_aruco_calib.create.marker:main',
+        "console_scripts": [
+            "is-aruco-calib-intrinsic=is_aruco_calib.calibrate.intrinsic:main",
+            "is-aruco-calib-extrinsic=is_aruco_calib.calibrate.extrinsic:main",
+            "is-aruco-calib-marker=is_aruco_calib.create.marker:main",
         ],
     },
     zip_safe=False,
@@ -23,5 +23,6 @@ setup(
         "is-wire==1.2.1",
         "numpy==1.24.4",
         "opencv-contrib-python==4.8.0.76",
+        "six==1.16.0",
     ],
 )
